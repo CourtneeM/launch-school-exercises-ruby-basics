809bd53a00dd7044fe32a923cd4524c6a51f8e6c
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-01-10 19:51:41 -0600

    First commit

---
+++ Reading Documentation/all_exercises.rb
@@ -0,0 +1,26 @@
+# 1.
+# https://docs.ruby-lang.org/en/3.0.0/Array.html OR https://ruby-doc.org/
+
+# 2.
+# https://ruby-doc.org/core-2.6/doc/syntax/control_expressions_rdoc.html#label-while+Loop
+
+# 3.
+# The result of a while loop is nil unless break is used to supply a value.
+
+# 4.
+# https://ruby-doc.org/core-2.6/doc/syntax/control_expressions_rdoc.html#label-break+Statement
+# break accepts the value of the argument if an argument is provided, otherwise it returns nil
+
+# 5.
+# https://ruby-doc.org/core-2.5.1/doc/syntax/literals_rdoc.html#label-Numbers
+# numbers can be written with underscores to improve readability
+
+# 6.
+# :first_name
+
+# 7.
+# https://ruby-doc.org/core-3.0.0/String.html
+
+# 8.
+# https://ruby-doc.org/core-3.0.0/String.html#method-i-rjust
+# #rjust
